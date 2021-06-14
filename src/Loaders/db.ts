a06f14fdadfff534e019c631b4be7460e5de4db9
jokj624 <jokj624@gmail.com>
2021-06-14 17:04:28 +0900

    feat: 모델 생성, db 연결

---
+++ src/Loaders/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import config from "../config"; 
+import User from "../models/User";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.mongoURI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Mongoose Connected ...");
+    User.createCollection().then(function(collection){
+      console.log("User Collection is created!");
+    });
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
