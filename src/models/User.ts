adbded9b3bf396876a8e9bef071e3d295d4c9bac
jokj624 <jokj624@gmail.com>
2021-06-16 17:20:47 +0900

    fix: Model required 수정

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,16 +4,13 @@ import { IUser } from "../interfaces/IUser";
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
     },
     email: {
         type: String,
-        required: true,
         unique: true,
     },
     password: {
         type: String,
-        required: true,
     },
     avatar: {
         type: String,
