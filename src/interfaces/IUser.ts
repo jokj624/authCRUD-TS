dbe6c000e9f73774965d4e74c872a65a4ec386c0
jokj624 <jokj624@gmail.com>
2021-06-16 17:19:07 +0900

    fix: User Interface 추가

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -10,4 +10,10 @@ export interface IUserInputDTO {
     name: string;
     email: string;
     password: string;
+    avatar?: string;
+    date?: Date;
+}
+
+export interface userUniqueSearchInput {
+    email : string;
 }
